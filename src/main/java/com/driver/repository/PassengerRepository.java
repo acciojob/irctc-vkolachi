19f214b64c2d74cee4a0ac09b794427069f76a31
vkola <vkolachi@gmail.com>
2023-10-04 15:00:58 +0530

    customer

--- src/main/java/com/driver/repository/PassengerRepository.java
+++ src/main/java/com/driver/repository/PassengerRepository.java
@@ -2,7 +2,9 @@ package com.driver.repository;
 
 import com.driver.model.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PassengerRepository extends JpaRepository<Passenger,Integer> {
 
 }
